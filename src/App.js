bf3d37c5bad473a910060a6e7415aef584a46096
teklu <teklufa@gmail.com>
2023-02-27 16:58:44 +0300

    display number of clients connected in real time

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react'
 import io from 'socket.io-client'
 const socketio_server = process.env.REACT_APP_SOCKETIO_SERVER_URL
 const socket = io(socketio_server)
 
 const App = () => {
+  const [ isConnected, setIsConnected ] = useState(socket.connected)
+  const [ clientCount, setClientCount ] = useState(0)
+
+  useEffect(() => {
+
+    socket.on('connect', () => {
+      setIsConnected(true)
+    })
+    socket.on('disconnect', () => {
+      setIsConnected(false)
+    })
+
+    socket.on('new connection', params => {
+      setClientCount(params.count)
+    })
+    socket.on('client left', ({ count }) => {
+      setClientCount(count)
+    })
+
+    // registered listeners must be cleanedup, to prevent multiple event listener
+    // registration, on the next rendering
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+      socket.off('new connection')
+      socket.off('client left')
+    }
+  }, [])
+
   return (
     <div>
-      hello
+      <p>Client count: { clientCount }</p>
     </div>
   );
 }
